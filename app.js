dff4b2b00b4cd1654f1830ade669534d638ca6f5
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-03-01 23:50:20 +0100

    js part complete

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const API_Key = `dcb232ebf34cc8accace5ab08e1916e3`;
 
 const searchTemperature = () => {
   const city = document.getElementById("city-name").value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_Key}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => displayTemperature(data));
+  document.getElementById("city-name").value = "";
 };
 
+const setInnerText = (id, text) => {
+  document.getElementById(id).innerText = text;
+};
 const displayTemperature = (temperature) => {
-  console.log(temperature);
+  setInnerText("city", temperature.name);
+  setInnerText("temperature", temperature.main.temp);
+  setInnerText("condition", temperature.weather[0].main);
+  // set weather icon
+  const url = ` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+  const imgIcon = document.getElementById("weather-icon");
+  imgIcon.setAttribute("src", url);
 };
